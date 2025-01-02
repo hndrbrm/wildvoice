12503d81a13f8d240a66d46ed9fb7648dbe6f4b2
hndrbrm <hndrbrm@gmail.com>
2025-01-02 23:57:38 +0700

    Initial Files.

---
+++ bin/wildvoice.dart
@@ -0,0 +1,5 @@
+import 'package:wildvoice/wildvoice.dart' as wildvoice;
+
+void main(List<String> arguments) {
+  print('Hello world: ${wildvoice.calculate()}!');
+}
---
+++ test/wildvoice_test.dart
@@ -0,0 +1,8 @@
+import 'package:wildvoice/wildvoice.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
